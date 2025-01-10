2519a91d50f49601a0fdc6cfae207218fea1887e
Dharanidharan K <86405846+Dharanidharan9791@users.noreply.github.com>
2025-01-10 07:17:53 +0530

    updated run.py

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 import os
-from app import app
 
 if __name__ == "__main__":
-   # port = int(os.environ.get("PORT",5000))  # Default to 5000 if PORT is not set
-   #  print("port",port)
-   app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
